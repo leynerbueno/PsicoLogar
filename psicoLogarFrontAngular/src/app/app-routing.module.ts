e2b2253bb28e346491ce43d9692647fc6de66e3b
Andre Chimicatti Fabrini <C0007186@vli-logistica.com.br>
2020-11-01 00:42:32 -0300

    Tela Lista de Pacientes

--- psicoLogarFrontAngular/src/app/app-routing.module.ts
+++ psicoLogarFrontAngular/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PagCadastroComponent } from './pag-cadastro/pag-cadastro.component';
-import {PagPsicologoComponent} from './pag-psicologo/pag-psicologo.component';
+import { PagPsicologoComponent } from './pag-psicologo/pag-psicologo.component';
+import { ListaPacientesComponent } from './lista-pacientes/lista-pacientes.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'cadastro/usuario', component: PagCadastroComponent},
-  {path: 'psicologo', component: PagPsicologoComponent}
+  {path: 'psicologo', component: PagPsicologoComponent},
+  {path: 'listaPacientes', component: ListaPacientesComponent}
 ];
 
 @NgModule({
