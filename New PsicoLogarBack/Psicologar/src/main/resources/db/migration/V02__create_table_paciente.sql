8f3843182ebc511523de12e312506c67773a656c
Juan Barcelos <juanbarcelos.ti@outlook.com>
2020-11-20 13:43:13 -0300

    Add insrt na migration

--- New PsicoLogarBack/Psicologar/src/main/resources/db/migration/V02__create_table_paciente.sql
+++ New PsicoLogarBack/Psicologar/src/main/resources/db/migration/V02__create_table_paciente.sql
@@ -11,3 +11,7 @@ data_da_consulta date not null,
 psicologo_id int,
 constraint FK_paciente Foreign key(psicologo_id) references psicologo(id)
 );
+
+INSERT INTO `psicologar`.`paciente` (`nome`, `data_de_nascimento`, `genero`, `email`, `senha`, `endereco`, `data_da_consulta`, `psicologo_id`) VALUES ('Ricardo', '2010-05-20', 'Masculino', 'ricardo@ricardo.com', '123', 'rua teste 04', '2020-08-10', '1');
+INSERT INTO `psicologar`.`paciente` (`nome`, `data_de_nascimento`, `genero`, `email`, `senha`, `endereco`, `data_da_consulta`, `psicologo_id`) VALUES ('Michele', '2000-01-20', 'Femenino', 'michele@michele.com', '123', 'rua teste 05', '2020-09-01', '2');
+INSERT INTO `psicologar`.`paciente` (`nome`, `data_de_nascimento`, `genero`, `email`, `senha`, `endereco`, `data_da_consulta`, `psicologo_id`) VALUES ('Fernanda', '1999-05-10', 'Femenino', 'fernanda@fernanda.com', '123', 'rua teste', '2020-06-13', '3');
--- New PsicoLogarBack/Psicologar/src/main/resources/db/migration/V03__create_table_diario.sql
+++ New PsicoLogarBack/Psicologar/src/main/resources/db/migration/V03__create_table_diario.sql
@@ -13,3 +13,7 @@ data_do_diario date not null,
 paciente_id int,
 constraint FK_diario Foreign key(paciente_id) references paciente(id)
 );
+
+INSERT INTO `psicologar`.`diario` (`emocao1`, `emocao2`, `emocao3`, `emocao4`, `emocao5`, `emocao_geral`, `diario_paciente`, `data_do_diario`, `paciente_id`) VALUES ('6', '7', '9', '5', '2', '29', 'teste apenas', '2020-08-01', '1');
+INSERT INTO `psicologar`.`diario` (`emocao1`, `emocao2`, `emocao3`, `emocao4`, `emocao5`, `emocao_geral`, `diario_paciente`, `data_do_diario`, `paciente_id`) VALUES ('8', '2', '6', '7', '1', '24', 'teste apenas', '2020-06-01', '2');
+INSERT INTO `psicologar`.`diario` (`emocao1`, `emocao2`, `emocao3`, `emocao4`, `emocao5`, `emocao_geral`, `diario_paciente`, `data_do_diario`, `paciente_id`) VALUES ('10', '10', '10', '10', '10', '50', 'teste apenas', '2020-08-15', '3');
